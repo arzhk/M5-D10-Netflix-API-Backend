89fe22052044b8ef3a444e3e3695bf8192a56829
arzhk <72197750+arzhk@users.noreply.github.com>
2020-12-18 12:37:00 +0000

    Added functionality
    - Added GET route for services/imdbsearch with query functionality for title and ability to filter by year and/or type

--- src/services/index.js
+++ src/services/index.js
@@ -12,11 +12,28 @@ const upload = multer({});
 
 router.get("/imdbsearch", async (req, res, next) => {
   try {
-    const response = await axios({
-      method: "get",
-      url: `${process.env.OMDB_BASE_URL + "/?i=" + req.query.id + process.env.OMDB_API_KEY}`,
-    });
-    res.send(response.data);
+    if (req.query.id) {
+      const response = await axios({
+        method: "get",
+        url: `${process.env.OMDB_BASE_URL + "/?i=" + req.query.id + process.env.OMDB_API_KEY}`,
+      });
+      res.send(response.data.Search);
+    } else if (req.query.title) {
+      const response = await axios({
+        method: "get",
+        url: `${process.env.OMDB_BASE_URL + "/?s=" + req.query.title + process.env.OMDB_API_KEY}`,
+      });
+      let data = response.data.Search;
+      if (req.query.year && req.query.type) {
+        data = data.filter((movie) => movie.Year === req.query.year);
+        data = data.filter((movie) => movie.Type === req.query.type);
+      } else if (req.query.year) {
+        data = data.filter((movie) => movie.Year === req.query.year);
+      } else if (req.query.type) {
+        data = data.filter((movie) => movie.Type === req.query.type);
+      }
+      res.send(data);
+    }
   } catch (error) {
     console.log(error);
     next(error);
