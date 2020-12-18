c40e44058146082f1d0402d360e75ba93108a382
arzhk <72197750+arzhk@users.noreply.github.com>
2020-12-18 14:22:16 +0000

    Added functionality
    - Added GET route for services/catalogue/export?title= which exports a PDF after fetching by the title query.

--- src/services/index.js
+++ src/services/index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const multer = require("multer");
 const fs = require("fs");
-const uniqid = require("uniqid");
 const path = require("path");
-const { check, validationResult } = require("express-validator");
 const { join } = require("path");
 const axios = require("axios").default;
+const pdfMake = require("pdfmake");
+const fonts = {
+  Roboto: {
+    normal: join(__dirname, "fonts/Roboto-Regular.ttf"),
+    bold: join(__dirname, "fonts/Roboto-Medium.ttf"),
+    italics: join(__dirname, "fonts/Roboto-Italic.ttf"),
+    bolditalics: join(__dirname, "fonts/Roboto-MediumItalic.ttf"),
+  },
+};
 
 const router = express.Router();
 const upload = multer({});
+
+// ********* GLOBAL FUNCTIONS
 
 const readMovieFileHandler = async (filename) => {
   const targetFile = JSON.parse(fs.readFileSync(join(__dirname, "../movies/", filename)).toString());
@@ -23,6 +32,40 @@ const writeMovieFileHandler = (writeToFilename, file) => {
     throw new Error(error);
   }
 };
+
+const errorMessage = (value, message, param = "_id", url = "url") => {
+  const err = new Error();
+  err.message = {
+    errors: [
+      {
+        value: value,
+        msg: message,
+        param: param,
+        location: url,
+      },
+    ],
+  };
+  err.httpStatusCode = 400;
+  return err;
+};
+
+const sendEmail = async (recipient) => {
+  try {
+    const msg = {
+      to: recipient,
+      from: process.env.SENDER_EMAIL, // Use the email address or domain you verified above
+      subject: "this is a test email",
+      text: `blablablabla NEW`,
+      html: `<strong>blablabla NEW</strong>`,
+    };
+    const res = await sgMail.send(msg);
+    console.log(res);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// ******** START OF ROUTERS
 
 router.get("/imdbsearch", async (req, res, next) => {
   try {
@@ -87,6 +130,65 @@ router.get("/sort", async (req, res, next) => {
       }
     } else {
       const error = errorMessage("Sort by query is missing");
+      next(error);
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.get("/catalogue/export", async (req, res, next) => {
+  try {
+    if (req.query.title) {
+      const response = await axios({
+        method: "get",
+        url: `${process.env.OMDB_BASE_URL + "/?s=" + req.query.title + process.env.OMDB_API_KEY}`,
+      });
+      const data = response.data.Search;
+      console.log(data);
+      const docDefinition = {
+        pageSize: "A4",
+        pageMargins: [40, 60, 40, 60],
+        permissions: {
+          printing: "highResolution",
+          modifying: false,
+          copying: false,
+          annotating: false,
+          fillingForms: false,
+          contentAccessibility: false,
+        },
+        header: { text: "Catalogue Request", fontSize: 22, bold: true },
+
+        content: [
+          { text: `Searched for... ${req.query.title}`, fontSize: 18 },
+          {
+            alignment: "justify",
+            columns: [
+              {
+                width: "30%",
+                text: "Response:",
+              },
+            ],
+          },
+          {
+            alignment: "justify",
+            columns: [
+              {
+                width: "80%",
+                text: JSON.stringify(data, null, 1),
+              },
+            ],
+          },
+        ],
+      };
+      const pdfFile = new pdfMake(fonts);
+      const pdfDoc = pdfFile.createPdfKitDocument(docDefinition);
+      res.setHeader("Content-Type", "application/pdf");
+      pdfDoc.pipe(res);
+      pdfDoc.end();
+    } else {
+      const error = errorMessage(`Title query is missing.`);
       next(error);
     }
   } catch (error) {
